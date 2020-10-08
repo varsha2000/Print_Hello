84f830b8ea800d7cd381775e9a949f1f8db7120e
allanjoston <72342102+allanjoston@users.noreply.github.com>
2020-10-08 20:46:14 +0530

    Create hw.c

---
+++ C/hw.c
@@ -0,0 +1,7 @@
+//A simple program to display Hello World using C.
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World !!!......");
+   return 0;
+}
